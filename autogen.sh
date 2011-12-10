4005caca77ede622e87cb06169fe1de90fa7178a
hellekin <hellekin@cepheide.org>
2011-12-10 10:23:26 +0100

    v0.4.0 embryo -- zsh power
    I removed everything else, so we can start adding stuff we really want ;o)

--- autogen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# autogen.sh -- Prepare the source tree for the configure script
-#
-# FIXME: check dependencies
-# FIXME: license
-
-autoreconf 2>/dev/null
-automake -ac && autoreconf
